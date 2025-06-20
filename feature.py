9787be7a4eed2507ab3c565ac98d5afd2e422094
zhaoxs686 <119672490+zhaoxs686@users.noreply.github.com>
2025-06-20 20:02:38 +0800

    Add files via upload

---
+++ feature.py
@@ -0,0 +1,44 @@
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+
+def mol2alt_sentence(mol, radius):
+
+    """
+    Parameters
+    ----------
+    mol : rdkit.Chem.rdchem.Mol
+    radius : float
+        Fingerprint radius
+
+    Returns
+    -------
+    list
+        alternating sentence
+    combined
+    """
+    radii = list(range(int(radius) + 1))
+    info = {}
+    _ = AllChem.GetMorganFingerprint(mol, radius, bitInfo=info)  # info: dictionary identifier, atom_idx, radius
+
+    mol_atoms = [a.GetIdx() for a in mol.GetAtoms()]
+
+    #     print(mol_atoms)
+    dict_atoms = {x: {r: None for r in radii} for x in mol_atoms}
+
+    for element in info:
+        for atom_idx, radius_at in info[element]:
+            dict_atoms[atom_idx][radius_at] = element  # {atom number: {fp radius: identifier}}
+
+    # merge identifiers alternating radius to sentence: atom 0 radius0, atom 0 radius 1, etc.
+    identifiers_alt = []
+    for atom in dict_atoms:  # iterate over atoms
+        for r in radii:  # iterate over radii
+            identifiers_alt.append(dict_atoms[atom][r])
+
+    alternating_sentence = map(str, [x for x in identifiers_alt if x])
+
+    return list(alternating_sentence)
